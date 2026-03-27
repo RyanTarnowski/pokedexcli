e26f135da11151f17e6f093c5a8ffc09e8da8f7f
Ryan Tarnowski <ryan.tarnowski@gmail.com>
2026-03-27 07:12:16 -0600

    Implement startRepl

--- repl.go
+++ repl.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if scanner.Scan() {
+			clean_user_input := cleanInput(scanner.Text())
+
+			if len(clean_user_input) > 0 {
+				fmt.Printf("Your command was: %s\n", clean_user_input[0])
+			}
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	scrubbed_text := strings.ToLower(text)
